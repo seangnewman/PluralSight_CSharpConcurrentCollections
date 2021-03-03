174f04e3bcac14d963eafe2a1c3ffa0d09cc743e
seangnewman <seangnewman@msn.com>
2021-03-03 06:39:48 -0700

    Add project files.

---
+++ SubmitOrders/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubmitOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            #region Adding Concurrency
+            //var ordersQueue = new Queue<string>();
+            //PlaceOrders(ordersQueue, "Cody", 5);
+            //PlaceOrders(ordersQueue, "Connor", 5);
+            var ordersQueue = new ConcurrentQueue<string>();
+            Task task1 = Task.Run( () => PlaceOrders(ordersQueue, "Cody", 5)); 
+            Task task2 = Task.Run( () => PlaceOrders(ordersQueue, "Connor", 5));
+
+            Task.WaitAll(task1, task2);
+            #endregion
+            foreach (var order in ordersQueue)
+            {
+                Console.WriteLine("ORDER : " + order);
+            }
+        }
+
+        private static void PlaceOrders(ConcurrentQueue<string> orders, string customerName, int nOrders)
+        {
+            for (int i = 0; i < nOrders; i++)
+            {
+                Thread.Sleep(new TimeSpan(1));
+                string orderName = $"{customerName} wants t-shirt {i}";
+                orders.Enqueue(orderName);
+            }
+        }
+    }
+}
