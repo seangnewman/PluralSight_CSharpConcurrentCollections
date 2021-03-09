22b4bc78d24c78c5c1b64fdc4338213f00710132
seangnewman <seangnewman@msn.com>
2021-03-09 06:32:49 -0700

    Review of Enumerating Collections and why you cannot rely on the results

---
+++ EnumerateDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace EnumerateDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new ConcurrentDictionary<string, int>();
+
+            stock.TryAdd("kcdc", 0);
+            stock.TryAdd("PluralSight Live 2018", 0);
+            stock.TryAdd("docker", 0);
+
+            foreach (var shirt in stock.ToArray())
+            {
+                stock["kcdc"]++;   
+                Console.WriteLine(shirt.Key + ":" + shirt.Value);
+            }
+        }
+    }
+}
